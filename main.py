cc8dfe1c480557adb209fc1a0fb1dff4b844d3dc
gnosinporta <56847249+gnosinporta@users.noreply.github.com>
2022-09-06 21:45:54 -0300

    game almost finished
    Now the snake grows everytime it eats, and the collision with itself and the walls is detected, trioggering the end of the game. There are some minor bugs to fix.

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from snake import Snake
 from scoreboard import Scoreboard
 import time
 
+MARGIN = 285
 
 # screen creation
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("my snake game")
-# the next instruction turns the animation on/and off 
+# the next instruction turns the animation on/off 
 # and sets delays for update drawings.
 # screen.update() must be called to see the effect
 screen.tracer(0)    
@@ -36,15 +37,21 @@ while game_is_on:
     snake.move()
 
     # detecting collision with food
-    if snake.head.distance(food) < 10:
-        print("om nom nom")
+    if snake.head.distance(food) < 15:
+        # print("om nom nom")
         scoreboard.increase_score()
         food.refresh()
+        snake.grow()
 
-    # TODO: detect collision with edges of the screen
-    # TODO: of course the snake must grow everytime it eats food
-    # TODO: detect collision with itself
-    # TODO: show game over in screen
+    # detectiung collision with walls
+    if snake.head.xcor() > MARGIN or snake.head.xcor() < -MARGIN or snake.head.ycor() > MARGIN or snake.head.ycor() < -MARGIN:
+        game_is_on = False
+        scoreboard.game_over() 
 
+    # detecting collision with itself
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over() 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -26,12 +26,16 @@ class Snake:
     # snake body creation
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
+    # adds a new segment to the snake
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    
     # snake moves
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -41,7 +45,11 @@ class Snake:
 
         self.segments[0].forward(MOVE_DISTANCE)
 
-    # all these methods define how the snake turns
+    # this function adds another segment to the snake everytime it eats
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
+
+    # all these following methods define how the snake turns
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
