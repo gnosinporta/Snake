4449fb963e8683bb150d63d95a3f5bc29a467356
gnosinporta <56847249+gnosinporta@users.noreply.github.com>
2022-10-06 03:02:13 -0300

    refactoring
    now every value is a constant...

--- food.py
+++ food.py
@@ -1,12 +1,18 @@
 from turtle import Turtle
 import random
 
-LENGHT = 0.5
-WIDTH = 0.5
+# CONSTANTS
+FOOD_LEN = 0.5
+FOOD_WID = 0.5
+FOOD_SHAPE = "circle"
+FOOD_COLOR = "red"
+FOOD_SPEED = "fastest"
+MARGIN = 280
+
 
 # TODO: the food must be created in a place where the snake is not present
 # TODO: the food must be created in a grid of 20x20 squares
-# TODO: Sometimes the food gets created in some specific pixels 
+# Sometimes the food gets created in some specific pixels
 # where the snake can not touch it. i made a little QA with the 
 # basic game so far. the 20x20 grid should sort out the problem 
 # aforementioned. (2/9/22)
@@ -17,15 +23,15 @@ class Food(Turtle):
         super().__init__()
 
         # creation of the ball of food
-        self.shape("circle")
-        self.color("red")
-        self.speed("fastest")
-        self.shapesize(stretch_len = LENGHT, stretch_wid = WIDTH)
+        self.shape(FOOD_SHAPE)
+        self.color(FOOD_COLOR)
+        self.speed(FOOD_SPEED)
+        self.shapesize(stretch_len=FOOD_LEN, stretch_wid=FOOD_WID)
         self.penup()
         self.refresh()
 
     def refresh(self):
         # creation of the random coordinates of the food
-        x_random = random.randint(-280, 280)
-        y_random = random.randint(-280, 280)
+        x_random = random.randint(-MARGIN, MARGIN)
+        y_random = random.randint(-MARGIN, MARGIN)
         self.goto(x_random, y_random)
--- main.py
+++ main.py
@@ -4,28 +4,34 @@ from snake import Snake
 from scoreboard import Scoreboard
 import time
 
+# CONSTANTS
 MARGIN = 285
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_BG_COLOR = "black"
+SCREEN_TITLE = "snake"
 
 # screen creation
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("my snake game")
-# the next instruction turns the animation on/off 
-# and sets delays for update drawings.
-# screen.update() must be called to see the effect
-screen.tracer(0)    
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.bgcolor(SCREEN_BG_COLOR)
+screen.title(SCREEN_TITLE)
 
+# the next instruction turns the animation on/off and sets delays for update drawings.
+# screen.update() must be called to see the effect
+screen.tracer(0)
+
+# creation of the main elements of the game
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# call to catch the keyboard commands
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 # game loop
 
@@ -43,15 +49,16 @@ while game_is_on:
         food.refresh()
         snake.grow()
 
-    # detectiung collision with walls
-    if snake.head.xcor() > MARGIN or snake.head.xcor() < -MARGIN or snake.head.ycor() > MARGIN or snake.head.ycor() < -MARGIN:
+    # detecting collision with walls
+    if snake.head.xcor() > MARGIN or snake.head.xcor() < -MARGIN or snake.head.ycor() > MARGIN or \
+            snake.head.ycor() < -MARGIN:
         game_is_on = False
-        scoreboard.game_over() 
+        scoreboard.game_over()
 
     # detecting collision with itself
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
-            scoreboard.game_over() 
+            scoreboard.game_over()
 
 screen.exitonclick()
